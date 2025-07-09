b35eb74330c6697a7561e22ee7688d13c6dece1e
Uttam Kumar <uk89197@gmail.com>
2025-07-09 10:46:36 +0530

    [Uttam Kumar] Add . Code to be commited for removing conflict from the branch DSA

--- Programming Elements/AverageMarksCalculator.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class AverageMarksCalculator{
-   public static void main(String args[]){
-       int maths = 94;
-       int physics  = 95;
-       int chemistry = 96;
-       System.out.println("Sam's average mark in PCM is: " + (maths + physics + chemistry)/3 + "%");
-   }
-}
